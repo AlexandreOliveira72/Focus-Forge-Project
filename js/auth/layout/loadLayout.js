3e297be7c24e6cf72200452c12533d97d83dbae2
AlexandreOliveira72 <alexandrenorman7@gmail.com>
2026-06-04 20:32:49 -0300

    Add revamped Sidebar and Top bar + New icons

--- js/auth/layout/loadLayout.js
+++ js/auth/layout/loadLayout.js
@@ -1,22 +1,28 @@
 async function loadLayout() {
+
     const response = await fetch("/components/layout.html");
     const layout = await response.text();
 
-    console.log(layout.length);
-
     const container = document.getElementById("layout-container");
 
-    if (container) {
+    if(container){
         container.innerHTML = layout;
     }
+
+    initLayout();
+}
+
+function initLayout(){
+    console.log("Layout carregado!");
 }
 
 loadLayout();
 
 function toggleSidebar() {
+
     const sidebar = document.getElementById("sidebar");
 
-    if (!sidebar) {
+    if(!sidebar){
         return;
     }
 
